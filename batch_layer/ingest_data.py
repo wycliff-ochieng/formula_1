d085597f785b4644472d530a1fcbcfd0e9447004
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-05-15 12:11:52 +0300

    laps

--- batch_layer/ingest_data.py
+++ batch_layer/ingest_data.py
@@ -6,6 +6,7 @@ import csv
 def get_car_data():
     car_data = requests.get("https://api.openf1.org/v1/car_data?session_key=9159&speed>=315")
     result_json = json.loads(car_data.text)
+
     with open("data/raw/cars.csv", mode="w",newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
         
@@ -34,7 +35,29 @@ def get_driver():
             for row in drivers_json:
                 csv_writer.writerow(row.values())
 
+    print("successfully loaded drivers data")
+
 get_driver()
+
+def get_laps():
+    laps = requests.get("https://api.openf1.org/v1/laps?session_key=9161")
+    laps_json = json.loads(laps.text)
+    print(laps_json)
+
+    with open("data/raw/laps.csv",mode="w",newline="") as csvfile:
+        csv_writer = csv. writer(csvfile)
+
+        if laps_json and isinstance(laps_json, list):
+            header = laps_json[0].keys()
+            csv_writer.writerow(header)
+
+            for row in laps_json:
+                csv_writer.writerow(row.values())
+    print("sucessfully loaded data")
+
+get_laps()
+
+
 
 #print(drivers_json)
 
